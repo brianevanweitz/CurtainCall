5350166f5bfa8ce260ccffc7a33903b38591a69b
Brian Weitz <brianweitz@Brians-MacBook-Pro-2.local>
2019-08-12 14:01:48 -0400

    auth, react init

--- CCServer/app/models/user.rb
+++ CCServer/app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  # has_secure_password
- # validates :email, presence: true, uniqueness: true
- # validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
- # validates :password, length: { minimum: 6 }
+ has_secure_password
+ validates :email, presence: true, uniqueness: true
+ validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+ validates :password, length: { minimum: 6 }
  has_many :matches
  has_many :matched_users, through: :matches
 
